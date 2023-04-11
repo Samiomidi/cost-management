18491b8432c0cb240e825793e6a57a4c0ad21cf5
Sami Omidi <samiomidi@hotmail.com>
2023-04-11 05:20:30 +0430

    search input changed

--- components/layout/Topbar.js
+++ components/layout/Topbar.js
@@ -8,53 +8,55 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-// import InputLabel from "@mui/material/InputLabel";
-// import MenuItem from "@mui/material/MenuItem";
-// import FormControl from "@mui/material/FormControl";
-// import Select from "@mui/material/Select";
 
 function Topbar() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
-  // const [selectedTheme, setSelectedTheme] = useState("dark");
-
-  // const handleChange = (event) => {
-  //   colorMode.toggleColorMode(event.target.value);
-  // };
+  const [showSearchInput, setShowSearchInput] = useState({
+    width: "0px",
+    visibility: "hidden",
+    backgroundColor: "",
+  });
   return (
-    <Box display="flex" justifyContent="space-between" p={2}>
+    <Box display="flex" justifyContent="flex-end" p={2}>
       {/* Search Bar */}
       <Box
         display="flex"
-        backgroundColor={colors.primary[400]}
         borderRadius="3px"
+        backgroundColor={showSearchInput.backgroundColor}
       >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton type="button" sx={{ p: 1 }}>
+        <InputBase
+          sx={{
+            ml: 2,
+            flex: 1,
+            width: showSearchInput.width,
+            visibility: showSearchInput.visibility,
+            transition: "0.3s all ease-in-out",
+            backgroundColor: colors.primary[400],
+          }}
+          placeholder="Search"
+        />
+        <IconButton
+          type="button"
+          sx={{ p: 1 }}
+          onClick={() =>
+            setShowSearchInput(
+              showSearchInput.width === "0px"
+                ? {
+                    width: "200px",
+                    visibility: "visible",
+                    backgroundColor: colors.primary[400],
+                  }
+                : { width: "0px", visibility: "hidden", backgroundColor: "" }
+            )
+          }
+        >
           <SearchIcon />
         </IconButton>
       </Box>
       {/* Icons */}
       <Box display="flex">
-        {/* <FormControl sx={{ m: 1, minWidth: 80 }}>
-          <InputLabel id="demo-simple-select-autowidth-label">Theme</InputLabel>
-          <Select
-            labelId="demo-simple-select-autowidth-label"
-            id="demo-simple-select-autowidth"
-            value={selectedTheme}
-            onChange={handleChange}
-            autoWidth
-            label="Theme"
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={"dark"}>Dark</MenuItem>
-            <MenuItem value={"goz"}>Goz</MenuItem>
-            <MenuItem value={"light"}>Light</MenuItem>
-          </Select>
-        </FormControl> */}
         <IconButton onClick={colorMode.toggleColorMode}>
           {theme.palette.mode === "dark" ? (
             <DarkModeOutlinedIcon />
