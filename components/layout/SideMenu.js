bd3b5c94adeaf13055225c90767c551c6c44be47
Sami Omidi <samiomidi@hotmail.com>
2023-04-12 06:12:49 +0430

    sidemenu in mobile changed

--- components/layout/SideMenu.js
+++ components/layout/SideMenu.js
@@ -89,214 +89,213 @@ function SideMenu({ menuOnClick }) {
 
   return (
     <Fragment>
-      {isShowSidebar && (
-        <Sidebar
+      <Sidebar
+        rootStyles={{
+          visibility: isShowSidebar ? "visible" : "hidden",
+          position: isMobile ? "fixed" : "relative",
+          transition: "0.5s all ease-in",
+          zIndex: 1000,
+          overflowY: "visible",
+          right: "auto",
+          left: 0,
+          height: "100vh",
+          display: "block",
+        }}
+        defaultCollapsed={isCollapsed}
+        backgroundColor={`${colors.primary[400]} !important`}
+        width="250px"
+        collapsedWidth="80px"
+        transitionDuration={200}
+      >
+        <Menu
           rootStyles={{
-            position: isCollapsed ? null : "fixed",
-            zIndex: 1000,
-            overflowY: "visible",
-            right: "auto",
-            left: 0,
-            height: "100vh",
-            display: "block",
-          }}
-          defaultCollapsed={isCollapsed}
-          backgroundColor={`${colors.primary[400]} !important`}
-          width="250px"
-          collapsedWidth="80px"
-          transitionDuration={200}
-        >
-          <Menu
-            rootStyles={{
-              [`.${menuClasses.button}`]: {
-                "&:hover": {
-                  color:
-                    theme.palette.mode === "dark"
-                      ? "#6870fa !important"
-                      : `${colors.calm[400]} !important`,
-                  backgroundColor: "transparent !important",
-                },
-              },
-              [`.${menuClasses.active}`]: {
+            [`.${menuClasses.button}`]: {
+              "&:hover": {
                 color:
                   theme.palette.mode === "dark"
                     ? "#6870fa !important"
                     : `${colors.calm[400]} !important`,
                 backgroundColor: "transparent !important",
               },
-            }}
-          >
-            {/* LOGO AND MENU ICON */}
-            <MenuItem>
-              {!isCollapsed && (
-                <Box
-                  display="flex"
-                  justifyContent="space-between"
-                  alignItems="center"
-                  ml="2rem"
-                >
-                  <Typography variant="h3" color={colors.gray[100]}>
-                    ADMIN
-                  </Typography>
-                </Box>
-              )}
-            </MenuItem>
-
+            },
+            [`.${menuClasses.active}`]: {
+              color:
+                theme.palette.mode === "dark"
+                  ? "#6870fa !important"
+                  : `${colors.calm[400]} !important`,
+              backgroundColor: "transparent !important",
+            },
+          }}
+        >
+          {/* LOGO AND MENU ICON */}
+          <MenuItem>
             {!isCollapsed && (
-              <Box mb="25px">
-                <Box display="flex" justifyContent="center" alignItems="center">
-                  <Image
-                    alt="profile-user"
-                    width={100}
-                    height={100}
-                    src="/assets/admin.jpg"
-                    style={{ cursor: "pointer", borderRadius: "50%" }}
-                  />
-                </Box>
-                <Box textAlign="center">
-                  <Typography
-                    variant="h2"
-                    color={colors.gray[100]}
-                    fontWeight="bold"
-                    sx={{ m: "10px 0 0 0" }}
-                  >
-                    Sami Omidi
-                  </Typography>
-                  <Typography
-                    variant="h5"
-                    color={
-                      theme.palette.mode === "dark"
-                        ? colors.secondary[500]
-                        : "#6870fa"
-                    }
-                  >
-                    VP Fancy Admin
-                  </Typography>
-                </Box>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="2rem"
+              >
+                <Typography variant="h3" color={colors.gray[100]}>
+                  ADMIN
+                </Typography>
               </Box>
             )}
+          </MenuItem>
 
-            <Box>
-              <Item
-                title="Dashboard"
-                to="/dashboard"
-                icon={<HomeOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <Image
+                  alt="profile-user"
+                  width={100}
+                  height={100}
+                  src="/assets/admin.jpg"
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.gray[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Sami Omidi
+                </Typography>
+                <Typography
+                  variant="h5"
+                  color={
+                    theme.palette.mode === "dark"
+                      ? colors.secondary[500]
+                      : "#6870fa"
+                  }
+                >
+                  VP Fancy Admin
+                </Typography>
+              </Box>
+            </Box>
+          )}
 
-              <Typography
-                variant="h6"
-                color={colors.gray[300]}
-                sx={{ m: "15px 0 5px 20px" }}
-              >
-                Data
-              </Typography>
-              <Item
-                title="Expenses"
-                to="/expenses"
-                icon={<TrendingDownOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              <Item
-                title="Incomes"
-                to="/incomes"
-                icon={<TrendingUpOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              <Item
-                title="Manage Team"
-                to="/team"
-                icon={<PeopleOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              <Item
-                title="Contacts Information"
-                to="/contacts"
-                icon={<ContactsOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              <Item
-                title="Invoices Balances"
-                to="/invoices"
-                icon={<ReceiptOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
+          <Box>
+            <Item
+              title="Dashboard"
+              to="/dashboard"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
 
-              <Typography
-                variant="h6"
-                color={colors.gray[300]}
-                sx={{ m: "15px 0 5px 20px" }}
-              >
-                Pages
-              </Typography>
-              <Item
-                title="Profile Form"
-                to="/form"
-                icon={<PersonOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              {/* <Item
+            <Typography
+              variant="h6"
+              color={colors.gray[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Data
+            </Typography>
+            <Item
+              title="Expenses"
+              to="/expenses"
+              icon={<TrendingDownOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Incomes"
+              to="/incomes"
+              icon={<TrendingUpOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Manage Team"
+              to="/team"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Contacts Information"
+              to="/contacts"
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Invoices Balances"
+              to="/invoices"
+              icon={<ReceiptOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.gray[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Pages
+            </Typography>
+            <Item
+              title="Profile Form"
+              to="/form"
+              icon={<PersonOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            {/* <Item
             title="Calendar"
             to="/calendar"
             icon={<CalendarTodayOutlinedIcon />}
             selected={selected}
             setSelected={setSelected}
           /> */}
-              <Item
-                title="FAQ Page"
-                to="/faq"
-                icon={<HelpOutlineOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
+            <Item
+              title="FAQ Page"
+              to="/faq"
+              icon={<HelpOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
 
-              <Typography
-                variant="h6"
-                color={colors.gray[300]}
-                sx={{ m: "15px 0 5px 20px" }}
-              >
-                Charts
-              </Typography>
-              <Item
-                title="Bar Chart"
-                to="/bar"
-                icon={<BarChartOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              <Item
-                title="Pie Chart"
-                to="/pie"
-                icon={<PieChartOutlineOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              <Item
-                title="Line Chart"
-                to="/line"
-                icon={<TimelineOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-              <Item
-                title="Geography Chart"
-                to="/geography"
-                icon={<MapOutlinedIcon />}
-                selected={selected}
-                setSelected={setSelected}
-              />
-            </Box>
-          </Menu>
-        </Sidebar>
-      )}
-
+            <Typography
+              variant="h6"
+              color={colors.gray[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Charts
+            </Typography>
+            <Item
+              title="Bar Chart"
+              to="/bar"
+              icon={<BarChartOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Pie Chart"
+              to="/pie"
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Line Chart"
+              to="/line"
+              icon={<TimelineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Geography Chart"
+              to="/geography"
+              icon={<MapOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </Sidebar>
       {!isCollapsed
         ? !isMobile && (
             <IconButton
